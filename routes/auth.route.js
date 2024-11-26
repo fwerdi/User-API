c99a78301d930492ba3c8ef26f8b7d72b29aad15
fwerdi <149984400+fwerdi@users.noreply.github.com>
2024-11-26 14:37:18 +0700

    Delete routes directory

--- routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { loginUser, registerUser } = require("../controllers/auth.controller");
-const router = express.Router();
-
-router.post("/login", loginUser);
-router.post("/register", registerUser);
-
-module.exports = router
